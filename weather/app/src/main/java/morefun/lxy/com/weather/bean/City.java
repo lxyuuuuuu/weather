3db4d37ade7f56e26cd0cb47d8e9eeec71f2e21f
lxyuuuuuu <352394037@qq.com>
2019-01-04 22:03:20 +0800

    first commit

---
+++ weather/app/src/main/java/morefun/lxy/com/weather/bean/City.java
@@ -0,0 +1,36 @@
+package morefun.lxy.com.weather.bean;
+
+/**
+ * Created by Kurt on 2018/11/15 下午2:50
+ */
+
+public class City {
+
+
+    private String city_code;
+    private String city_name;
+
+    public String getCity_code() {
+        return city_code;
+    }
+
+    public void setCity_code(String city_code) {
+        this.city_code = city_code;
+    }
+
+    public String getCity_name() {
+        return city_name;
+    }
+
+    public void setCity_name(String city_name) {
+        this.city_name = city_name;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "city_code='" + city_code + '\'' +
+                ", city_name='" + city_name + '\'' +
+                '}';
+    }
+}
